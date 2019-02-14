c4875afcd41a8f0a39f6e35980d91e1f5fb61e2f
Olga <olga.shrir@gmail.com>
2019-02-14 12:36:55 +0200

    init, init canvas, editor-panel

---
+++ JavaScript/editor-controller.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var gCanvas;
+var gCtx;
+
+
+function init(){
+    initCanvas()
+    renderGallery();
+}
+
+function initCanvas(){
+    gCanvas = document.querySelector('#canvas');
+    gCtx = gCanvas.getContext('2d');
+    gCanvas.width = 800;
+    gCanvas.height = 300;
+    // gCanvas.width = 100%;
+    // gCanvas.heigth = 100%;
+
+    var img = getImage();
+    img.src = '../leo.jpg';  
+    img.onload = function(){
+        gCtx.drawImage(img, 0, 0);
+    }
+
+}
+
