f4bd74ee570584b98abffe1c6bd64f5a678f37b5
Olga <olga.shrir@gmail.com>
2019-02-14 20:44:51 +0200

    renderCanvas, renderText, onChangeFontSize

--- JavaScript/editor-controller.js
+++ JavaScript/editor-controller.js
@@ -1,27 +1,45 @@
 'use strict'
 
-var gCanvas;
-var gCtx;
-
-
-function init(){
-    initCanvas()
+function init() {
+    // init canvas
+    gCanvas = document.querySelector('#canvas');
+    gCtx = gCanvas.getContext('2d');
+    renderCanvas();
     renderGallery();
 }
 
-function initCanvas(){
-    gCanvas = document.querySelector('#canvas');
-    gCtx = gCanvas.getContext('2d');
-    gCanvas.width = 800;
-    gCanvas.height = 300;
-    // gCanvas.width = 100%;
-    // gCanvas.heigth = 100%;
 
-    var img = getImage();
-    img.src = '../leo.jpg';  
-    img.onload = function(){
-        gCtx.drawImage(img, 0, 0);
-    }
-
+function renderCanvas(url) {
+    // TODO: to render picked image from gallery
+    var img = document.querySelector('.test')
+    gCanvas.width = img.width;
+    gCanvas.height = img.height;
+    // img.src = url;
+    initContext();
+    gCtx.drawImage(img, 0, 0);
 }
 
+
+function renderText() {
+    renderCanvas(); 
+
+    var ctx = getGCtx();
+    var text = getGText();
+
+    // text settings
+    text.top = document.querySelector('.textTop').value;
+    text.bottom = document.querySelector('.textBottom').value;
+
+    // write top text
+    gCtx.fillText(text.top, gCanvas.width / 2, 40);
+    gCtx.strokeText(text.top, gCanvas.width / 2, 40);
+
+    // write bottom text
+    gCtx.fillText(text.bottom, gCanvas.width / 2, gCanvas.height -10);
+    gCtx.strokeText(text.bottom, gCanvas.width / 2, gCanvas.heigth - 10);
+}
+
+function onChangeFontSize(sign){ 
+    if (sign === 'plus') editContextFontSize(10)
+    else editContextFontSize(-10)
+}
--- JavaScript/editor-service.js
+++ JavaScript/editor-service.js
@@ -1,9 +1,48 @@
 'use strict'
 
+var gText = {top: '', bottom: ''};
+var gCanvas;
+var gCtx;
 
-function getImage(){
-    var img = new Image();
-    img.src = '../leo.jpg'; 
 
-    return img;
+function initContext() {
+    gCtx.font = "50px Impact";
+    gCtx.textAlign="center";
+    gCtx.fillStyle  = "white";
+    gCtx.strokeStyle  = "#black";
+}
+
+function editContextFontSize(change){
+    var size = ''
+    size = '' + gCtx.font.charAt(0) + gCtx.font.charAt(1);
+    size = parseInt(size);
+
+    console.log('change:', change);
+    
+    var num = size + change;
+
+    console.log('gCtx.font:', gCtx.font);
+    gCtx.font = `${num}px Impact`;
+    console.log('gCtx.font:', gCtx.font);
+
+    renderText();
+}
+
+
+// function getImage(){
+//     var img = new Image();
+//     img.src = '../leo.jpg'; 
+//     return img;
+// }
+
+function getGCtx(){
+    return gCtx;
+}
+
+function getGCanvas(){
+    return gCanvas;
+}
+
+function getGText(){
+    return gText;
 }
